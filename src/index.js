0186cbb041f58c1d7dff831aa35753a90faad626
Melvin Carvalho <melvincarvalho@gmail.com>
2025-12-31 11:21:14 +0100

    chore: prepare solid-chat v0.0.1 for npm publishing
    - Rename package to solid-chat
    - Add exports for longChatPane and chatListPane
    - Add src/index.js barrel export
    - Add peerDependencies for rdflib
    - Add files array for npm

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// solid-chat - Modern chat panes for Solid pods
+// https://solid-chat.com
+
+export { longChatPane } from './longChatPane.js'
+export { chatListPane, addChat, removeChat, updateChatPreview } from './chatListPane.js'
